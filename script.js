03e728db8c938194fbfa26848116168a3d796028
Gaurav <gauravgurung065@gmail.com>
2021-07-21 22:19:19 +0545

    add

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '✔ correct number';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 18;
+// document.querySelector('.guess').value = 8;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⚠ No Number!';
+  }
+});
